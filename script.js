eb67915f11f946f49124c284bf9244dbc8fabd04
Ritika Saha <94214071+ritikasaha2002@users.noreply.github.com>
2024-12-31 21:28:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const div = document.getElementById("innercontainer");
+function display(){
+    div.innerHTML = "<div style='background-color:black;padding-left:40px'><h5>Happy New Year 2025.&#127881</h5></div>"
+}
+
+setTimeout(display,6000);
